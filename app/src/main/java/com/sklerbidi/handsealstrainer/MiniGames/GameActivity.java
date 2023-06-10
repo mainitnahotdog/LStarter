f8dba728e8890e92b75a2bb4936e16d11059c8b0
John Mark Payas <sklerbidi.09@gmail.com>
2023-06-10 22:51:05 +0800

    committed as fck

---
+++ app/src/main/java/com/sklerbidi/handsealstrainer/MiniGames/GameActivity.java
@@ -0,0 +1,16 @@
+package com.sklerbidi.handsealstrainer.MiniGames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sklerbidi.handsealstrainer.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
